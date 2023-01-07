b76c974e8fb0cc1e2150276f7272e34c8a48600f
higa <codehiga@gmail.com>
2023-01-07 13:57:35 -0300

    feat: implementation either error on create

--- src/entities/Employee.ts
+++ src/entities/Employee.ts
@@ -1,24 +1,29 @@
-import { NewEmployeeDTO } from "../dto/new-employee-dto";
-import { Email } from "./Email";
+import { NewEmployeeDTO } from "@/dto/new-employee-dto";
+import { Either, left, right } from "@/shared/either";
+import { Email } from "./email";
+import { InvalidEmailError } from "./errors/invalid-email";
 
 export class Employee {
   public name: string;
-  public email: Email;
+  public email: string;
   public type: string;
 
-  constructor(name: string, email: Email, type: string) {
+  constructor(name: string, email: string, type: string) {
     this.name = name;
     this.email = email;
     this.type = type;
   }
 
-  static create({ name, email, type }: NewEmployeeDTO) {
-    let emailValid = Email.create(email);
-    if (emailValid) {
-      const employee = new Employee(name, emailValid, type);
-      return employee;
-    } else {
-      return null;
+  static create({
+    name,
+    email,
+    type,
+  }: NewEmployeeDTO): Either<InvalidEmailError, Employee> {
+    let createdEmail = Email.create(email);
+    if (createdEmail.isLeft()) {
+      return left(new InvalidEmailError());
     }
+    const employee = new Employee(name, email, type);
+    return right(employee);
   }
 }
