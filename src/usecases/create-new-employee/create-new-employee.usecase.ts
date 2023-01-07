c4f601f7527b81b9a1d5a75a98ada5870dd38fa6
higa <codehiga@gmail.com>
2023-01-07 17:00:18 -0300

    feat: create new employee usecase impl

---
+++ src/usecases/create-new-employee/create-new-employee.usecase.ts
@@ -0,0 +1,32 @@
+import { NewEmployeeDTO } from "@/dto/new-employee-dto";
+import { Employee } from "@/entities/employee";
+import { InvalidEmailError } from "@/entities/errors/invalid-email";
+import { InvalidNameError } from "@/entities/errors/invalid-name";
+import { EmployeeRepository } from "@/ports/employee-repository";
+import { Either, left } from "@/shared/either";
+
+export class CreateNewEmployee {
+  private repository: EmployeeRepository;
+
+  constructor(repository: EmployeeRepository) {
+    this.repository = repository;
+  }
+
+  async execute({
+    email,
+    name,
+    type,
+  }: NewEmployeeDTO): Promise<
+    Either<InvalidNameError | InvalidEmailError, void>
+  > {
+    const employeeOrError = Employee.create({
+      email,
+      name,
+      type,
+    });
+    if (employeeOrError.isLeft()) {
+      return left(employeeOrError.value);
+    }
+    await this.repository.save(employeeOrError.value);
+  }
+}
