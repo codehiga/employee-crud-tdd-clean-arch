7935516e8d9bacd567646f6b28db349994441012
higa <codehiga@gmail.com>
2023-01-07 18:03:41 -0300

    test: test usecase list-employees redeem all

---
+++ test/usecases/list-employees/list-employees.test.ts
@@ -0,0 +1,36 @@
+import { Employee } from "@/entities/employee";
+import { EmployeeRepository } from "@/ports/employee-repository";
+import { InMemoryEmployeeRepository } from "@/repository/in-memory-employee-repository";
+import { ListEmployees } from "@/usecases/list-employees/list-employees";
+
+describe("Test ListEmployee usecase", () => {
+  let repository: EmployeeRepository;
+
+  beforeAll(async () => {
+    const employees: Employee[] = [];
+    repository = new InMemoryEmployeeRepository(employees);
+    const employeeOrError = Employee.create({
+      email: "employee@test.com",
+      name: "employee",
+      type: "employee",
+    });
+    const employee2OrError = Employee.create({
+      email: "employee2@test.com",
+      name: "employee2",
+      type: "employee2",
+    });
+    if (employeeOrError.isRight()) {
+      await repository.save(employeeOrError.value);
+    }
+    if (employee2OrError.isRight()) {
+      await repository.save(employee2OrError.value);
+    }
+  });
+
+  it("should return all employees", async () => {
+    const listEmployeesUseCase = new ListEmployees(repository);
+    const result = await listEmployeesUseCase.execute();
+    expect(result.length).toBe(2);
+    expect(result[0].email).toBe("employee@test.com");
+  });
+});
