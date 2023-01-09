0ebf31f0736f5b02b7edb495cc8f1ac320f42b42
higa <codehiga@gmail.com>
2023-01-09 20:04:39 -0300

    fix: rename duplicated email error

--- src/repository/ports/employee-repository.ts
+++ src/repository/ports/employee-repository.ts
@@ -1,11 +1,11 @@
 import { Employee } from "@/entities/employee";
 import { UpdateEmployeeDTO } from "@/entities/ports/dto/update-employee-dto";
-import { DuplicatedEmailError } from "@/repository/errors/DuplicatedEmailError";
 import { UserToUpdateNotFoundError } from "@/repository/errors/user-to-update-not-found-error";
 import { Either } from "@/shared/either";
+import { DuplicatedEmailError } from "../errors/duplicated-email-error";
 
 export interface EmployeeRepository {
-  save(newEmployee: Employee): Promise<Either<DuplicatedEmailError, void>>;
+  save(newEmployee: Employee): Promise<Either<DuplicatedEmailError, Employee>>;
   findEmployeeByEmail(email: string): Promise<Employee | null>;
   findAll(): Promise<Employee[] | []>;
   update(
